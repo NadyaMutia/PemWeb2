d4efc230ca8fbde4f7402763685f422f7eea32f1
Nadya Mutia Safira <114472135+NadyaMutia@users.noreply.github.com>
2023-03-10 21:07:22 +0700

    Pekan 3

---
+++ Pekan 3/footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <p>Created by
+            <a href="">Nadya Mutia Safira &copy;2022</a>
+        </p>
+    </footer>
+</body>
+</html>
---
+++ Pekan 3/score.php
@@ -0,0 +1,7 @@
+<?php 
+    include_once 'header.php';
+?>
+    <h1>INI HALAMAN SCORE!</h1>
+<?php
+    require_once 'footer.php';
+?>
