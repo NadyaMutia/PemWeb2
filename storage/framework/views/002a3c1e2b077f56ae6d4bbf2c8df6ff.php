a1cd87f72a7ce271da138af73038b214163c719c
NadyaMutia <nadyamutia2@gmail.com>
2023-05-22 16:46:00 +0700

    Routing Laravel

---
+++ storage/framework/views/002a3c1e2b077f56ae6d4bbf2c8df6ff.php
@@ -0,0 +1,3 @@
+<h2>HALAMAN PROFIL USER</h2>
+
+Kategori User : <?= $kategoriuser; ?><?php /**PATH C:\laravel\ti13\resources\views/user/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f5cfb6cff24d11e4abe56806c4ddb23b.php
@@ -0,0 +1,14 @@
+<table border="1" width="100%"> 
+    <tr>
+        <th>Nama</th>
+        <th>Tanggal</th>
+        <th>Tanggal Lahir</th>
+        <th>Jenis Kelamin</th>
+    </tr>
+    <tr style="text-align: center">
+        <td> <?= $nama; ?>  </td>
+        <td> <?= $tanggal; ?>  </td>
+        <td> <?= $tanggal_lahir; ?>  </td>
+        <td> <?= $jk; ?>  </td>
+    </tr>
+</table><?php /**PATH C:\laravel\ti13\resources\views/periksa/data.blade.php ENDPATH**/ ?>
